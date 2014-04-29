32976196e4649b422bd67c569047e5dfb3c07ac5
solid-rock <solid-rock@tlen.pl>
2014-04-29 10:54:29 +0200

    lab7

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+var discountCalculator = require("../discount");
+
+describe("discount Calculator", function()
+{
+    it("should have discount function", function()
+    {
+        assert.equal(discountCalculator.discount instanceof Function, true,
+        "typeof discount: " + typeof discountCalculator.discount);
+    });
+});
