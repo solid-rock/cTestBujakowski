06c229418a5daf56612ae64fa35fa628ff2c4a08
solid-rock <solid-rock@tlen.pl>
2014-04-29 11:13:25 +0200

    lab7 p.7

--- test/test.js
+++ test/test.js
@@ -9,3 +9,12 @@ describe("discount Calculator", function()
         "typeof discount: " + typeof discountCalculator.discount);
     });
 });
+
+describe("discount function", function()
+{
+	it("discount should return 0 with no args ", function()
+	{
+		var discount = discountCalculator.discount();
+		assert.equal(discount, 0, discount + "");
+	});
+})
